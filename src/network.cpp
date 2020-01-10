b951e412b28f364163170b4e147156dd6d63ce3a
Exr0n <spotyie@gmail.com>
2020-01-09 18:37:12 -0800

    chore: sync changes across devices

--- src/network.cpp
+++ src/network.cpp
@@ -1,4 +1,27 @@
 #pragma once
 
+#include <vector>
+
 #include "matrix.cpp"
 #include "layer.cpp"
+#include "activation.cpp"
+
+/**
+ * Network - a general model framework
+ */ 
+class Network {
+  typedef float val_t;
+  typedef unsigned dim_t;
+
+  std::vector<Layer<val_t> > layers;
+  unsigned _epoch_size;
+  unsigned _status_log_count;
+public:
+  /**
+   * Default constructor
+   */
+  Network () {
+    _epoch_size = 500000;
+    _status_log_count = 100;
+  }
+}
