d186ecfc7515d8899e2c8e83519f2bc76e9772dd
Exr0n <spotyie@gmail.com>
2020-01-09 21:47:25 -0800

    working on network class

--- src/network.cpp
+++ src/network.cpp
@@ -16,12 +16,46 @@ class Network {
   std::vector<Layer<val_t> > layers;
   unsigned _epoch_size;
   unsigned _status_log_count;
+  dim_t _input_dim;
 public:
   /**
    * Default constructor
    */
-  Network () {
-    _epoch_size = 500000;
-    _status_log_count = 100;
+  Network() {}
+  /**
+   * "Normal" constructor
+   * @param input_dim Dimensionality of the input vector
+   */
+  Network(dim_t input_dim)
+  {
+    _input_dim = input_dim;
+  }
+
+  /**
+   * Adds a layer to the model
+   * @param out_dim Dimensionality of the output of this layer
+   */
+  void addLayer(dim_t out_dim)
+  {
+    dim_t prev = _input_dim;
+    if (layers.size()) prev = layers[layers.size()-1].out_size();
+    layers.push_back(Layer(prev, out_dim));
+  }
+
+  /**
+   * Train the network
+   * @param epoch_size How many iterations to train through
+   */
+  void train(const Matrix<val_t> &inp, Matrix<val_t> exp)
+  {
+    std::vector<Matrix<val_t> > snapshots;
+    snapshots.push_back(inp);
+
+    for (const Layer<val_t> &layer : layers)
+      snapshots.push_back(layer.feed(snapshots[snapshots.length()-1]));
+    snapshots.push_back(exp);
+
+    std::vector<val_t>::reverse_iterator rit = myvector.rbegin();
+    // TODO: BACKPROP AAAAA
   }
 }
